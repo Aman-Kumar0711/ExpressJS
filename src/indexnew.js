108a78c281f112f713a96b2ba609276e16ef3107
Aman <amanchahar175@gmail.com>
2024-07-25 22:15:34 +0530

    template engine ke baare mei jaana

---
+++ src/indexnew.js
@@ -0,0 +1,20 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+const port = 8000;
+//to set view engine
+app.set("view engine", "hbs");
+//agar ejs ya pug use karta yo hbs ki jagah unko likhta
+app.set("views", path.join(__dirname, "../views"));
+app.get("", (req, res) => {
+  res.render("index"); //hbs maine upar hi mention kar diya hai
+  //render se yeh hoga ki jo bhi file maine di hai woh render hogi, dynamically
+
+  //maan lo mujhe dynamic content bhejna ho to main index.hbs waali file mei {{something}} likhunga and uss something ko yahaan define kar dunga
+  //eg
+  //res.render("index",{
+  //   something:"alpha"})  to ab jahaan maine index.hbs mei something likha hoga wahaan alpha aa jaayega
+});
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
---
+++ templateEngine.md
@@ -0,0 +1,10 @@
+# Template Engine
+
+## About
+
+Jab bhi humko dynamic data ke saath deal karna hota hai we use template engine in express.js
+Kyun ki HTML mei tum dynamic data nahin rakh sakte iss se nipatne ke liye we use template engines.
+
+A template engine enables you to use static template files in your application. At runtime, the template engine replaces variables in a template file with actual values, and transforms the template into an HTML file sent to the client. This approach makes it easier to design an HTML page.
+
+Some popular template engines that work with Express are `Pug`, `Mustache`, and `EJS`. The Express application generator uses Jade as its default, but it also supports several others.
